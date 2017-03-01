5768cbaa4e4ded936bac851846beda7c52362874
Janet Zhang <jzhang12@stuy.edu>
2017-02-28 22:34:44 -0500

    done

--- dots.js
+++ dots.js
@@ -1,4 +1,5 @@
 var svg = document.getElementById("vimage");
+var cb = document.getElementById("clear");
 
 var started = false;
 var oldX = 0;
@@ -8,17 +9,31 @@ var oldY = 0;
 
 var addDot = function(e){
 	if (started) {
-		console.log("started");
+		console.log(started);
+		console.log(oldX,oldY);
 		var newDot = document.createElementNS( "http://www.w3.org/2000/svg", "circle");
 		newDot.setAttribute("cx",e.offsetX);
 		newDot.setAttribute("cy",e.offsetY);
 		newDot.setAttribute("r","10");
 		newDot.setAttribute("fill","goldenrod");
 		
-		document.getElementById("vimage").appendChild(newDot);
+
+		var newLine = document.createElementNS( "http://www.w3.org/2000/svg", "line");
+		newLine.setAttribute("x1",e.offsetX);
+		newLine.setAttribute("y1",e.offsetY);
+		newLine.setAttribute("x2",oldX);
+		newLine.setAttribute("y2",oldY);
+		newLine.setAttribute("stroke","black");
+
 		
+		document.getElementById("vimage").appendChild(newDot);
+		document.getElementById("vimage").appendChild(newLine);
+		oldX = e.offsetX;
+		oldY = e.offsetY;
+
 	}
 	else {
+		console.log(started);
 		var newDot = document.createElementNS( "http://www.w3.org/2000/svg", "circle");
 		newDot.setAttribute("cx",e.offsetX);
 		newDot.setAttribute("cy",e.offsetY);
@@ -26,8 +41,9 @@ var addDot = function(e){
 		newDot.setAttribute("fill","goldenrod");
 		
 		document.getElementById("vimage").appendChild(newDot);
-		var oldX = e.offsetX;
-		var oldY = e.offsetY;
+		oldX = e.offsetX;
+		oldY = e.offsetY;
+		console.log(oldX,oldY);
 
 		started = true;
 	};
@@ -35,5 +51,14 @@ var addDot = function(e){
 
 };
 
+var clearSVG = function(){
+	while (svg.lastChild) {
+		svg.removeChild(svg.lastChild);
+	};
+	started = false;
 
+};
+
+
+cb.addEventListener("click",clearSVG);
 svg.addEventListener("click", addDot);
